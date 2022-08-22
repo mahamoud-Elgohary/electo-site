542ffc45a9b4dc8643dec75a6639f212493c7670
mahamoud-Elgohary <111690764+mahamoud-Elgohary@users.noreply.github.com>
2022-08-23 00:08:54 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    $(".category-button").click(function () {
+        $(".category-button ul").slideToggle(500);
+    })
+});
+
+    
+var midswiper = new Swiper(".mid-swiper", {
+
+  // grabCursor: true,
+  // centeredSlidees: true,
+  slidesPerView: 3.5,
+  breakpoints: {  
+    '908': {
+      slidesPerView: 3.5,
+      spaceBetween: 20,},
+    '640': {
+      slidesPerView: 2.5,
+      spaceBetween: 10, },
+      '510': {
+        slidesPerView: 2,
+        spaceBetween: 10, },
+        '320': {
+          slidesPerView: 1.5,
+          spaceBetween: 10, },
+          '250': {
+            slidesPerView: 1,
+            spaceBetween: 10, },
+  },
+  
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".mid-swiper-next",
+    prevEl: ".mid-swiper-prev",
+  },
+  
+
+});
+
+
+var topswiper = new Swiper(".top-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // keyboard: {
+  //   enabled: true,
+  // },
+  loop: true,
+  pagination: {
+    el: ".top-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+$(document).ready(function () {
+  $(".sidebar-button").click(function () {
+      $(".sidebar").toggleClass('active');
+  })
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+      $('.to-the-top').fadeIn();
+  } else {
+      $('.to-the-top').fadeOut();
+  }
+});
+
+$(".to-the-top").click(function () {
+ //1 second of animation time
+ //html works for FFX but not Chrome
+ //body works for Chrome but not FFX
+ //This strange selector seems to work universally
+ $("html, body").animate({scrollTop: 0}, 500);
+});
